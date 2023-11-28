162e05173c2640798d7f60fdaf4294a4fc59b7f4
m-lm <31556425+m-lm@users.noreply.github.com>
2023-11-28 01:30:58 -0600

    fix leftover input when generating new problem

--- minmath/solve/static/problem_generator.js
+++ minmath/solve/static/problem_generator.js
@@ -1,5 +1,6 @@
 function generate() {
     // const > let > var for variable declarations
+    document.getElementById("math-input").value = ""; // clear input field
     let num1 = Math.floor(Math.random() * 100);
     let num2 = Math.floor(Math.random() * 100);
     let mathProblem = num1 + " + " + num2 + " = ";
