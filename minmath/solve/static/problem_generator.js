58f078f5906768ba99563e499c622c00de47ce8a
m-lm <31556425+m-lm@users.noreply.github.com>
2023-11-28 00:54:05 -0600

    implement basic addition problem generation in js

--- minmath/solve/static/problem_generator.js
+++ minmath/solve/static/problem_generator.js
@@ -1,5 +1,9 @@
 function generate() {
-   document.getElementById("math").innerHTML = "test"; 
+    // const > let > var for variable declarations
+    let num1 = Math.floor(Math.random() * 100);
+    let num2 = Math.floor(Math.random() * 100);
+    let mathProblem = num1 + " + " + num2 + " = ";
+    document.getElementById("math").innerHTML = mathProblem;
 }
 
 generate();
