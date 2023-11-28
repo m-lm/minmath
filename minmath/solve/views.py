6a9934647b24894ecd92c629c35cf4b7add87c6d
m-lm <31556425+m-lm@users.noreply.github.com>
2023-11-27 23:46:16 -0600

    fix template render of solve app

--- minmath/solve/views.py
+++ minmath/solve/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import render
-from django.template import loader
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    '''
-    context
-    template = loader.get_template("polls/index.html")
-    return HttpResponse(template.render(context, request))
-    '''
-    pass
+    return render(request, "solve/index.html") 
     
