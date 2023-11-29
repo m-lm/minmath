e8426e3e354fd7ada79700ddfc47b3055879f805
m-lm <31556425+m-lm@users.noreply.github.com>
2023-11-29 01:11:35 -0600

    add basic input handling and answer feedback

--- minmath/solve/static/answer_check.js
+++ minmath/solve/static/answer_check.js
@@ -1,3 +1,32 @@
-function checkAnswer() {
+function checkAnswer(ans)  {
+    // on valid (i.e., integer) input...
+    let current_prob = document.getElementById("math").textContent;
+    let components = current_prob.split(/\s+/); // regex split whitespace for numbers
+    let sum = 0;
+    for (let i = 0; i < components.length; i++) {
+        if (!isNaN(parseInt(components[i]))) {
+            sum += parseInt(components[i]);
+        }
+    }
+    if (ans == sum) {
+        document.getElementById("input-status").style.color = "green";
+        document.getElementById("input-status").textContent = "Correct!";
+    }
+
+    else {
+        document.getElementById("input-status").style.color = "red";
+        document.getElementById("input-status").textContent = "Incorrect!";
+    }
+}
+
+function validate() {
     let ans = document.getElementById("math-input").value;
+    if (isNaN(parseInt(ans))) {
+        document.getElementById("math-input").value = "";
+        document.getElementById("input-status").style.color = "red";
+        document.getElementById("input-status").textContent = "Invalid input!";
+    }
+    else {
+        checkAnswer(ans);
+    }
 }
--- minmath/solve/static/problem_generator.js
+++ minmath/solve/static/problem_generator.js
@@ -4,7 +4,7 @@ function generate() {
     let num1 = Math.floor(Math.random() * 100);
     let num2 = Math.floor(Math.random() * 100);
     let mathProblem = num1 + " + " + num2 + " = ";
-    document.getElementById("math").innerHTML = mathProblem;
+    document.getElementById("math").textContent = mathProblem;
 }
 
 generate();
