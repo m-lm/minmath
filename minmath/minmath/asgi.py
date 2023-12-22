240cb917186176bf6ec8c176aacdcf1adbb518a2
m-lm <31556425+m-lm@users.noreply.github.com>
2023-12-21 19:33:09 -0600

    tweak settings env var

--- minmath/minmath/asgi.py
+++ minmath/minmath/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'minmath.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'minmath.settings.base')
 
 application = get_asgi_application()
