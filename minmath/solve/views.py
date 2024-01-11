e209853bdc4145f6f42a0bec22ba9e5a5da28fb6
m-lm <31556425+m-lm@users.noreply.github.com>
2024-01-10 19:35:43 -0600

    add temp context to solve views

--- minmath/solve/views.py
+++ minmath/solve/views.py
@@ -4,5 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, "solve/index.html") 
+    return render(request, "solve/index.html", {"sitename": "minmath"}) 
+
     
