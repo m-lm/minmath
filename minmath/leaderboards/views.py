ed30e14c0a14c54783c3d4b7be6b6488b2893aae
m-lm <31556425+m-lm@users.noreply.github.com>
2024-01-15 01:42:43 -0600

    add leaderboards template

--- minmath/leaderboards/views.py
+++ minmath/leaderboards/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("This is the leaderboard index.")
+    return render(request, "leaderboards/leaderboards.html")
