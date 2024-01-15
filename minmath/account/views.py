64196ad72ef338c0af1ab4b729fcb2206dee55c3
m-lm <31556425+m-lm@users.noreply.github.com>
2024-01-15 02:45:31 -0600

    add temporary dummy register form and remove font

--- minmath/account/views.py
+++ minmath/account/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def register(request):
-    return render(request, "account/register.html")
+    form = UserCreationForm()
+    return render(request, "account/register.html", {"form":form})
