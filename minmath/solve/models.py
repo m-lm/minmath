345ed7054344e1a2136e45cd50f102f5b06cb8a5
m-lm <31556425+m-lm@users.noreply.github.com>
2024-01-15 01:17:52 -0600

    tweak model and view files, including minigame model for solve app

--- minmath/solve/models.py
+++ minmath/solve/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# Models being akin to database tables
+
+class Minigame(models.Model):
+    # Model for each minigame session with end score and time
+    # Used for leaderboards, tied to user
+    score = models.IntegerField()
+    time = models.FloatField()
