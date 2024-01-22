df159ee63d7808b8098884b5e07d6a543292f15a
m-lm <31556425+m-lm@users.noreply.github.com>
2024-01-22 01:04:50 -0600

    tweak minigame model

--- minmath/solve/models.py
+++ minmath/solve/models.py
@@ -8,9 +8,12 @@ class Minigame(models.Model):
     # Model for each minigame session with end score and time
     # Used for leaderboards, tied to user
     # Leaderboards: username, gamemode, score, time, total num of problems, date
-    user = models.ForeignKey(PlayerUser, on_delete = models.CASCADE) # on deletion of obj, delete those references that depend on it
-    gamemode = models.CharField(max_length = 20)
+    user = models.ForeignKey(PlayerUser, on_delete=models.CASCADE) # on deletion of obj, delete those references that depend on it
+    gamemode = models.CharField(max_length=20)
     score = models.IntegerField()
     time_spent = models.FloatField()
     total_problems = models.IntegerField()
-    date = models.DateField(); # for activity records
+    date = models.DateField(auto_now=True); # for activity records
+
+    def __str__(self):
+        return (f"{self.user}'s {self.gamemode} game on {date}")
