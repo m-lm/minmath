3d34b3d1beddc68ba720def1dce40ed6f72f2ac8
m-lm <31556425+m-lm@users.noreply.github.com>
2024-01-22 00:49:20 -0600

    add default mode loading and rename mode button ids

--- minmath/solve/static/mode_select.js
+++ minmath/solve/static/mode_select.js
@@ -1,13 +1,18 @@
 function select_mode(id) {
-    act_btn = document.getElementById(id);
+    let act_btn = document.getElementById(id);
     act_btn.style.color = "var(--name-hov)";
     localStorage.setItem("mode", id);
-    btn_list = document.getElementsByClassName("mode-button");
-    num_btns = btn_list.length;
+    let btn_list = document.getElementsByClassName("mode-button");
+    let num_btns = btn_list.length;
     for (let i = 0; i < num_btns; i++) {
         if (btn_list[i].id != id) {
             btn_list[i].style.color = "";
         }
     }
+}
+
+if (localStorage.getItem("mode") == "") {
+    // if no mode has been previously set or mode has been reset by browser
+    localStorage.setItem("mode", "add");
 }
 window.addEventListener("load", select_mode(localStorage.getItem("mode")))
--- minmath/solve/static/problem_generator.js
+++ minmath/solve/static/problem_generator.js
@@ -3,6 +3,7 @@ function generate() {
     document.getElementById("math-input").value = ""; // clear input field
     let num1 = Math.floor(Math.random() * 100);
     let num2 = Math.floor(Math.random() * 100);
+    let current_mode = localStorage.getItem("mode");
     let mathProblem = num1 + " + " + num2;
     document.getElementById("math").textContent = mathProblem;
 }
