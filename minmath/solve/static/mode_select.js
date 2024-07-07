5b74891c87cdd885a71d6d90d359bb8d5c895f73
m-lm <31556425+m-lm@users.noreply.github.com>
2024-07-06 20:57:32 -0500

    refactor frontend mode select + localstorage

--- minmath/solve/static/mode_select.js
+++ minmath/solve/static/mode_select.js
@@ -1,28 +1,31 @@
-function select_mode(id) {
+function selectMode(id) {
     let mdBtn = document.getElementById(id);
+    let btnList;
     mdBtn.style.color = "var(--accent)";
-    localStorage.setItem("mode", id);
     if (mdBtn.className == "mode-button md1") {
-        let btnList = document.getElementsByClassName("mode-button md1");
-        for (let i = 0; i < btnList.length; i++) {
-            if (btnList[i].id != id) {
-                btnList[i].style.color = "";
-            }
-        }
+        btnList = document.getElementsByClassName("mode-button md1");
+        localStorage.setItem("mode1", id);
     }
     else if (mdBtn.className == "mode-button md2") {
-        let btnList = document.getElementsByClassName("mode-button md2");
-        for (let i = 0; i < btnList.length; i++) {
-            if (btnList[i].id != id) {
-                btnList[i].style.color = "";
-            }
+        btnList = document.getElementsByClassName("mode-button md2");
+        localStorage.setItem("mode2", id);
+    }
+    for (let i = 0; i < btnList.length; i++) {
+        if (btnList[i].id != id) {
+            btnList[i].style.color = "";
         }
     }
 }
 
-if (localStorage.getItem("mode") == "") {
+if (localStorage.getItem("mode1") == "") {
     // if no mode has been previously set or mode has been reset by browser
-    localStorage.setItem("mode", "add");
+    localStorage.setItem("mode1", "add");
 }
 
-window.addEventListener("load", select_mode(localStorage.getItem("mode")))
+if (localStorage.getItem("mode2") == "") {
+    // if no mode has been previously set or mode has been reset by browser
+    localStorage.setItem("mode2", "normal");
+}
+
+window.addEventListener("load", selectMode(localStorage.getItem("mode1")))
+window.addEventListener("load", selectMode(localStorage.getItem("mode2")))
