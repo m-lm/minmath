7c642817ec5b5502f8c7eab8a4a71c2546e15ba3
m-lm <31556425+m-lm@users.noreply.github.com>
2024-07-07 00:00:40 -0500

    small tweaks

--- minmath/solve/static/problem-gen.js
+++ minmath/solve/static/problem-gen.js
@@ -3,7 +3,7 @@ function generate() {
     document.getElementById("math-input").value = ""; // clear input field
     let num1 = Math.floor(Math.random() * 100);
     let num2 = Math.floor(Math.random() * 100);
-    let curMode = localStorage.getItem("mode");
+    let curMode = localStorage.getItem("mode1");
     let mathProblem = num1 + " + " + num2;
     document.getElementById("math").textContent = mathProblem;
 }
