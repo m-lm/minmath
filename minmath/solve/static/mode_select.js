5ba1a2b69573994f7da8a40de469a50e3b6b577b
m-lm <31556425+m-lm@users.noreply.github.com>
2024-07-06 20:39:21 -0500

    add separate mode interaction on landing

--- minmath/solve/static/mode_select.js
+++ minmath/solve/static/mode_select.js
@@ -2,11 +2,22 @@ function select_mode(id) {
     let act_btn = document.getElementById(id);
     act_btn.style.color = "var(--accent)";
     localStorage.setItem("mode", id);
-    let btn_list = document.getElementsByClassName("mode-button");
-    let num_btns = btn_list.length;
-    for (let i = 0; i < num_btns; i++) {
-        if (btn_list[i].id != id) {
-            btn_list[i].style.color = "";
+    if (act_btn.className == "mode-button md1") {
+        let btn_list = document.getElementsByClassName("mode-button md1");
+        let num_btns = btn_list.length;
+        for (let i = 0; i < num_btns; i++) {
+            if (btn_list[i].id != id) {
+                btn_list[i].style.color = "";
+            }
+        }
+    }
+    else if (act_btn.className == "mode-button md2") {
+        let btn_list = document.getElementsByClassName("mode-button md2");
+        let num_btns = btn_list.length;
+        for (let i = 0; i < num_btns; i++) {
+            if (btn_list[i].id != id) {
+                btn_list[i].style.color = "";
+            }
         }
     }
 }
@@ -15,4 +26,5 @@ if (localStorage.getItem("mode") == "") {
     // if no mode has been previously set or mode has been reset by browser
     localStorage.setItem("mode", "add");
 }
+
 window.addEventListener("load", select_mode(localStorage.getItem("mode")))
