41f927e236a6b62fb670c0f118c068152b09ab67
m-lm <31556425+m-lm@users.noreply.github.com>
2024-07-08 18:40:45 -0500

    add session bg color and clean up some stuff

--- minmath/account/urls.py
+++ minmath/account/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "account"
 
 urlpatterns = [
-    path("", views.register, name = "profile"),
+    path("", views.profile, name = "profile"),
     path("register/", views.register, name = "register"),
     path("profile/", views.profile, name = "profile"),
     path("logout/", views.logout_view, name = "logout"),
