9f654511db1ba0a93b019106e70af61daa6668a3
m-lm <31556425+m-lm@users.noreply.github.com>
2024-07-07 20:37:55 -0500

    add error checking for user auth when accessing login/reg

--- minmath/account/urls.py
+++ minmath/account/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LogoutView
 
 app_name = "account"
 
 urlpatterns = [
-    #path("", views.register, name = "register"),
+    path("", views.register, name = "profile"),
     path("register/", views.register, name = "register"),
     path("profile/", views.profile, name = "profile"),
     path("logout/", views.logout_view, name = "logout"),
