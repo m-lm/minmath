c22918d98d666a6b3d6957b8f18dc5d0f14b26e1
m-lm <31556425+m-lm@users.noreply.github.com>
2024-07-13 13:31:35 -0500

    separate progress bar functionality from timer, tweak styling

--- minmath/solve/static/game-load.js
+++ minmath/solve/static/game-load.js
@@ -1,54 +1,32 @@
-/*
-function streak() {
-    // solve within 3 seconds
-    // since as of now there is no "wrong" feedback,
-    // if you get it correct it just goes next
-    let display = document.getElementById("mode-status");
-    let count = 0;
-    let obsNode = document.getElementById("math");
-    const config = { characterData: true };
-    const grace = 3; // hard code 3 second grace period
-    display.innerHTML = "Streak: " + count;
-    let timer = setInterval(() => {
-        let time = 0;
-        const observer = new MutationObserver(mutations => {
-            for (const mutation of mutations) {
-                if (mutation.type === "characterData") {
-                    count++;
-                    console.log("npgw/i3s");
-                }
-            }
-        });
-        observer.observe(obsNode, config);
-        time++;
-        if (time >= grace) {
-            clearInterval(timer);
-        }
-        observer.disconnect();
-    }, 1000);
-}
-*/
-
 function timer() {
     let display = document.getElementById("mode-status");
-    let prog = document.getElementById("progress-bar");
     let time = Number(localStorage.getItem("duration"));
-    const rate = 100 / time;
-    let width = 0;
     display.innerHTML = "Time: " + time;
-    prog.style.visibility = "visible";
     let timer = setInterval(() => {
         // CARE: weird 1 second delay, so offset by 1; also adjust end condition as needed
         // need to double-check for backend data consistency/veracity
         display.innerHTML = "Time: " + (time - 1); 
         time--;
-        width += rate;
-        prog.style.width = (100 - width) + "%";
         if (time < 1) {
             clearInterval(timer);
-            prog.style.width = 0 + "%"; // force clear when time's up
         }
     }, 1000);
+}
+
+function progBar() {
+    let prog = document.getElementById("progress-bar");
+    const time = Number(localStorage.getItem("duration"));
+    const rate = 10 / time;
+    let width = 0;
+    prog.style.visibility = "visible";
+    let bar = setInterval(() => {
+        width += rate;
+        prog.style.width = (100 - width) + "%";
+        if (width >= 100) {
+            clearInterval(bar);
+            prog.style.width = 0 + "%"; // force clear when time's up
+        }
+    }, 100);
 }
 
 function zen() {
@@ -61,4 +39,5 @@ document.addEventListener("load", zen());
 
 if (localStorage.getItem("mode2") == "timed") {
     document.addEventListener("load", timer());
+    document.addEventListener("load", progBar());
 }
