7326d9e6bdc8b8968cbe899adb6f2c6c76b80390
m-lm <31556425+m-lm@users.noreply.github.com>
2024-07-22 18:42:11 -0500

    refactor answer validation

--- minmath/solve/static/answer-check.js
+++ minmath/solve/static/answer-check.js
@@ -2,18 +2,28 @@ import {generateProblem} from "./problem-gen.js";
 
 let score = 0;
 
-function checkAnswer(ans)  {
-    // Check that player input is valid (i.e., integer) input
-    let curProb = document.getElementById("math").textContent;
-    let components = curProb.split(/\s+/); // regex split whitespace for numbers
+function checkInput(ans) {
+    // Prevent weird input; also return Number type sum
+    if (isNaN(parseInt(ans[ans.length - 1]))) {
+        // if last char input is NaN then backtrack
+        document.getElementById("math-input").value = ans.substring(0, ans.length - 1);
+    }
+     // Check that player input is valid (i.e., integer) input
+    let curProblem = document.getElementById("math").textContent;
+    let components = curProblem.split(/\s+/); // regex split whitespace for numbers
     let sum = 0;
     for (let i = 0; i < components.length; i++) {
         if (!isNaN(parseInt(components[i]))) {
             sum += parseInt(components[i]);
         }
     }
-    // Correct answer
-    if (ans == sum) {
+    return sum;
+}
+
+function validateAnswer() {
+    let playerAns = document.getElementById("math-input").value;
+    let correctAns = checkInput(ans);
+    if (playerAns == correctAns) {
         document.getElementById("math-input").value = "";
         score++;
         document.getElementById("score").textContent = "Score: " + score;
@@ -21,15 +31,4 @@ function checkAnswer(ans)  {
     }
 }
 
-function validate() {
-    let ans = document.getElementById("math-input").value;
-    if (isNaN(parseInt(ans[ans.length - 1]))) {
-        // if last char input is NaN then backtrack
-        document.getElementById("math-input").value = ans.substring(0, ans.length - 1);
-    }
-    else {
-        checkAnswer(ans);
-    }
-}
-
-document.querySelector("input").addEventListener("input", validate);
+document.querySelector("input").addEventListener("input", validateAnswer);
