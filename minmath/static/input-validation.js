801fdbf8271cc14ec38a8d37a76fe02877dc1009
m-lm <31556425+m-lm@users.noreply.github.com>
2024-07-23 14:38:13 -0500

    fix input validation for pasting/dragging and insertions before last index

--- minmath/static/input-validation.js
+++ minmath/static/input-validation.js
@@ -1,11 +1,20 @@
 const inputElements = document.querySelectorAll("input");
 
 for (let i = 0; i < inputElements.length; i++) {
+    inputElements[i].setAttribute("onPaste", "return false");
+    inputElements[i].setAttribute("onDragStart", "return false");
+    inputElements[i].setAttribute("onDrag", "return false");
+    inputElements[i].setAttribute("onDrop", "return false");
+    inputElements[i].setAttribute("onCut", "return false");
+    inputElements[i].setAttribute("onCopy", "return false");
     inputElements[i].addEventListener("input", (e) => {
         let v = e.target.value;
-        if (isNaN(parseInt(v[v.length - 1]))) {
-            // if last char input is NaN then backtrack
-            e.target.value = v.substring(0, v.length - 1);
+        for (let j = 0; j < v.length; j++) {
+            if (isNaN(parseInt(v[j]))) {
+                let newVal = v.slice(0, j) + v.slice(j + 1);
+                e.target.value = newVal;
+                e.target.selectionStart = e.target.selectionEnd = j;
+            }
         }
     })
 }
