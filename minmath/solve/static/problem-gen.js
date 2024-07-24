249766b5110cd5b4c13531404f3703a86cee5f8c
m-lm <31556425+m-lm@users.noreply.github.com>
2024-07-23 22:29:12 -0500

    replace another if-else block with switch

--- minmath/solve/static/problem-gen.js
+++ minmath/solve/static/problem-gen.js
@@ -26,24 +26,25 @@ function generateProblem() {
     let nums = generateNums();
     let chosenMode = curModes[curModes.length * Math.random() | 0]; // randomize mode for problem
     // Depending on mode chosen, make sure answers are nonnegative integers
-    if (chosenMode == "sub") {
-        mathProblem = Math.max.apply(Math, nums) + symbols[chosenMode] + Math.min.apply(Math, nums);
-    }
-    else if (chosenMode == "divd") {
-        // If mode is divide, reroll a different mode to prevent divide by zero conflicts
-        if (nums.includes(0)) {
-            chosenMode = (curModes.length > 1) ? curModes.filter(x => x != "divd")[(curModes.length - 1) * Math.random() | 0] : ["add","sub","mult"][3 * Math.random() | 0]; 
-        }
-        else {
-            // CARE: 0 % 0 is undefined/NaN, see division by zero
-            while (Math.max.apply(Math, nums) % Math.min.apply(Math, nums) != 0) {
-                nums = generateNums();
+    switch (chosenMode) {
+        case "sub":
+            mathProblem = Math.max.apply(Math, nums) + symbols[chosenMode] + Math.min.apply(Math, nums);
+            break;
+        case "divd":
+            // If mode is divide, reroll a different mode to prevent divide by zero conflicts
+            if (nums.includes(0)) {
+                chosenMode = (curModes.length > 1) ? curModes.filter(x => x != "divd")[(curModes.length - 1) * Math.random() | 0] : ["add","sub","mult"][3 * Math.random() | 0]; 
             }
-        }
-        mathProblem = Math.max.apply(Math, nums) + symbols[chosenMode] + Math.min.apply(Math, nums);
-    }
-    else {
-        mathProblem = nums[0] + symbols[chosenMode] + nums[1];
+            else {
+                // CARE: 0 % 0 is undefined/NaN, see division by zero
+                while (Math.max.apply(Math, nums) % Math.min.apply(Math, nums) != 0) {
+                    nums = generateNums();
+                }
+            }
+            mathProblem = Math.max.apply(Math, nums) + symbols[chosenMode] + Math.min.apply(Math, nums);
+            break;
+        default:
+            mathProblem = nums[0] + symbols[chosenMode] + nums[1];
     }
     document.getElementById("math").textContent = mathProblem;
 }
