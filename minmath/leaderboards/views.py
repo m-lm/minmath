c6cbc00b4806bbaeb36273e52c922bb856a669ba
m-lm <31556425+m-lm@users.noreply.github.com>
2024-10-08 17:42:15 -0500

    fix improper number of games on leaderboard showing from 11 to 10

--- minmath/leaderboards/views.py
+++ minmath/leaderboards/views.py
@@ -5,15 +5,14 @@ from django.db.models import F, FloatField
 from django.db.models.functions import Cast
 
 def leaderboards(request):
-    highest = []
-    fastest = []
+    highest, fastest = [], []
     highest_games = Minigame.objects.order_by("-score")
     fastest_games = Minigame.objects.annotate(speed=Cast(F("score"), FloatField()) / Cast(F("time_duration"), FloatField())).order_by("-speed")
     for g in highest_games:
-        if len(highest) <= 10: # Only 10 games passed in
+        if len(highest) < 10: # Only 10 games passed in
             highest.append(g)
     for g in fastest_games:
-        if len(fastest) <= 10:
+        if len(fastest) < 10:
             fastest.append(g)
     data = {"highest_games": highest, 
             "fastest_games": fastest,}
