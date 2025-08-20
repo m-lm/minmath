0484700c281e2e257a390e677dfabe34daf6200e
m-lm <31556425+m-lm@users.noreply.github.com>
2025-08-19 21:45:58 -0500

    Enforce unique emails when registering new users

--- minmath/account/models.py
+++ minmath/account/models.py
@@ -4,5 +4,6 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class PlayerUser(AbstractUser):
+    email = models.EmailField(unique=True) # Enforce email uniqueness
     def __str__(self):
         return self.username
