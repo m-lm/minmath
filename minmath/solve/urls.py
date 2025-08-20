61aaf05e64dae41165bb340d3a737c15dea8ac44
m-lm <31556425+m-lm@users.noreply.github.com>
2025-08-19 21:02:32 -0500

    Fix redirect loop and refactor score passing for results

--- minmath/solve/urls.py
+++ minmath/solve/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("", views.landing, name = "landing"),
     path("game", views.game, name = "game"),
     path("info", views.info, name = "info"),
-    path("results", views.submit_score, name = "submit_score"),
+    path("submit_score", views.submit_score, name = "submit_score"),
     path("results", views.results, name = "results"),
 ]
--- minmath/solve/views.py
+++ minmath/solve/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse, HttpResponse, HttpResponseNotAllowed
 from .models import Minigame
 from .forms import MinigameForm
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 # Create your views here.
 
@@ -13,24 +14,23 @@ def game(request):
     return render(request, "solve/game.html")
 
 def results(request):
-    return render(request, "solve/results.html")
+    score = request.session.pop("score", None)
+    return render(request, "solve/results.html", {"score": f"Score: {score}"})
 
 def info(request):
     return render(request, "solve/info.html")
 
 def submit_score(request):
-    if not request.user.is_authenticated:
-        # Just redirect if user is not logged in
-        return redirect("solve:results")
+    score = None
     if request.method == "POST":
         form = MinigameForm(request.POST)
         if form.is_valid():
-            game = form.save(commit=False)
-            game.user = request.user
-            game.save()
-            print("SUCCESS:", game)
-            data = {"score": f"Score: {game.score}"}
-            return render(request, "solve/results.html", data)
-    else:
-        form = MinigameForm()
-    return redirect("solve:results")
+            score = form.cleaned_data.get("score")
+            if request.user.is_authenticated:
+                game = form.save(commit=False)
+                game.user = request.user
+                game.save()
+                print("SUCCESS:", game)
+            request.session["score"] = score
+        return redirect(reverse("solve:results"))
+    return redirect(reverse("solve:results"))
