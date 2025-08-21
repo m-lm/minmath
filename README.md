a85e0656a9c4e25c030bd6c40167e1411e9f821f
m-lm <31556425+m-lm@users.noreply.github.com>
2025-08-20 20:20:29 -0500

    Tweak settings for staticfiles management; tweak restart button styling

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # minmath
 
+[![Deploy to Render](https://api.render.com/deploy/button?repo=m-lm/minmath)](https://render.com/deploy)
+
 A website dedicated to mental math exercises with a focus on minimalism.
--- minmath/minmath/settings/base.py
+++ minmath/minmath/settings/base.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -55,9 +56,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'minmath.urls'
-
-# STATICFILES_DIRS manually added - analogous purpose to DIRS key in TEMPLATES
-STATICFILES_DIRS = ["static"]
 
 TEMPLATE_DIR = BASE_DIR / "templates"
 
@@ -124,8 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# STATICFILES_DIRS manually added - analogous purpose to DIRS key in TEMPLATES
+STATICFILES_DIRS = ["static"]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
